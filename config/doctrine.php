0a6400b995cf7f40fb6967a56c9dd46d59487bd4
ManonTfx <manon.trefoux@gmail.com>
2020-12-04 17:52:05 +0100

    correction

--- config/doctrine.php
+++ config/doctrine.php
@@ -1,18 +1,17 @@
-  
-<?php
+  <?php
 
-/**
- * Configuration file for Doctrine DBAL
- */
+    /**
+     * Configuration file for Doctrine DBAL
+     */
 
-return [
-    'connection' => [
-        // Put your database connection parameters here
-        // see the official documentation for more infos
-        // https://www.doctrine-project.org/projects/doctrine-dbal/en/latest/reference/configuration.html
-        'url' => 'mysql://root:root@127.0.0.1:8889/share_it'
-    ]
-];
+    return [
+        'connection' => [
+            // Put your database connection parameters here
+            // see the official documentation for more infos
+            // https://www.doctrine-project.org/projects/doctrine-dbal/en/latest/reference/configuration.html
+            'url' => 'mysql://root:root@127.0.0.1:8889/share_it'
+        ]
+    ];
 
 
 // mysql://root:@localhost:3306/share_it => pc
